f40e9476c9c09cf411e46f4b36ced54b1e96da05
Lem4ek <94479613+Lem4ek@users.noreply.github.com>
2025-07-31 23:09:58 +0300

    Update run.sh

--- BNK_BOT_2/run.sh
+++ BNK_BOT_2/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-export BOT_TOKEN=$(bashio::config 'bot_token')
+export 8199873882:AAE4x2ARLf7bR0fC9ykeOyHsrinT9JPIdRM=$(bashio::config 'bot_token')
 
 exec python3 /app/prod_report_bot.py
