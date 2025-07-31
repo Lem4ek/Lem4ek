778be70a9100136dfbcb107d0090751d6143185f
Lem4ek <94479613+Lem4ek@users.noreply.github.com>
2025-07-31 23:41:06 +0300

    Update prod_report_bot.py

--- BNK_BOT_2/prod_report_bot.py
+++ BNK_BOT_2/prod_report_bot.py
@@ -85,4 +85,13 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(main())
+    
